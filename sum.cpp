e128cf50d6711417fe95933eb3250acfa1c50257
Rishabh <rishabhsingh9861@gmail.com>
2023-02-26 10:59:13 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int  main() {
+
+int a , b , c ;
+cout<< "Enter the two number"; 
+cin>> a,b ;
+c=a+b;
+cout<<" The Sum is"<<c ;
+
+return 0 ;
+    
+}
+
